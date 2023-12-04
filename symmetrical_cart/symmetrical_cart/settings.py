681cbb955102083f6f6a27212f7bfa9f5f98ce6f
ddvuser <denisdoroshenko99@gmail.com>
2023-12-04 23:13:49 +0200

    create shop app

--- symmetrical_cart/symmetrical_cart/settings.py
+++ symmetrical_cart/symmetrical_cart/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop',
 ]
 
 MIDDLEWARE = [
@@ -114,6 +115,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+AUTH_USER_MODEL = "shop.CustomUser"
 
 
 # Static files (CSS, JavaScript, Images)
