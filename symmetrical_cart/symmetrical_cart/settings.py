be748c24917073905cf5d73d3bc5409df2d2c9aa
ddvuser <denisdoroshenko99@gmail.com>
2023-12-04 19:54:52 +0200

    add requirements, postgres

--- symmetrical_cart/symmetrical_cart/settings.py
+++ symmetrical_cart/symmetrical_cart/settings.py
@@ -12,24 +12,22 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv 
 
-
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env.dev')
+load_dotenv(dotenv_path)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY') 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', default=0) 
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(" ")
-
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
 
 # Application definition
 
@@ -75,7 +73,6 @@ WSGI_APPLICATION = 'symmetrical_cart.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
    'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
