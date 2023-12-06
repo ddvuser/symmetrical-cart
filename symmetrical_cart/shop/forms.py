e7decc66c0ad619c5817d75166def11ea232365f
ddvuser <denisdoroshenko99@gmail.com>
2023-12-06 02:33:38 +0200

    add register, login forms

--- symmetrical_cart/shop/forms.py
+++ symmetrical_cart/shop/forms.py
@@ -7,11 +7,21 @@ class RegisterForm(UserCreationForm):
         model = CustomUser
         fields = ['email', 'password1', 'password2']
 
-    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control',
+                                                           'autocomplete': 'off',
+                                                           'placeholder': 'Email'}))
+    password1 = forms.CharField(label='Password', 
+                                widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                  'autocomplete': 'off',
+                                                                  'placeholder': 'Password'}))
+    password2 = forms.CharField(label='Password confirmation', 
+                                widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                  'autocomplete': 'off',
+                                                                  'placeholder': 'Password confirmation'}))
 
 
 class LoginForm(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control',
+                                                            'placeholder': 'Email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                                                 'placeholder': 'Password'}))
