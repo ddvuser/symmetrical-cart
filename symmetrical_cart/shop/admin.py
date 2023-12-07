1d936496b9377a386d3f76c6dcd40d6d7aec5617
ddvuser <denisdoroshenko99@gmail.com>
2023-12-08 01:22:42 +0200

    add order model

--- symmetrical_cart/shop/admin.py
+++ symmetrical_cart/shop/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser, Category, Product
+from .models import CustomUser, Category, Product, Order
 
 admin.site.register(CustomUser)
 admin.site.register(Category)
 admin.site.register(Product)
+admin.site.register(Order)
--- symmetrical_cart/shop/models.py
+++ symmetrical_cart/shop/models.py
@@ -58,3 +58,27 @@ class Product(models.Model):
     def __str__(self):
         return f"Product: \"{self.name}\""
 
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    customer = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price = models.IntegerField()
+    address = models.CharField(max_length=50, default='', blank=True)
+    phone = models.CharField(max_length=50, default='', blank=True)
+    date = models.DateTimeField(default=timezone.now)
+
+    def place_order(self):
+        self.save()
+
+    @staticmethod
+    def get_orders_by_customer(customer_id):
+        return Order.objects.filter(customer=customer_id).order_by('-date')
+
+    def __str__(self):
+        return f"Order: \"{self.product.name}\""
+
+
+
+
+
+
