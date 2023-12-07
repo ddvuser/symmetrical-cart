bc2ae3c3decc4b244e755865a98be13d35c5d7de
ddvuser <denisdoroshenko99@gmail.com>
2023-12-08 00:48:12 +0200

    add category, product models

--- symmetrical_cart/shop/admin.py
+++ symmetrical_cart/shop/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, Category, Product
 
 admin.site.register(CustomUser)
+admin.site.register(Category)
+admin.site.register(Product)
--- symmetrical_cart/shop/models.py
+++ symmetrical_cart/shop/models.py
@@ -20,3 +20,41 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self) -> str:
         return self.email
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    @staticmethod
+    def get_all_categories():
+        return Category.objects.all()
+
+    def __str__(self):
+
+        return f"Category: \"{self.name}\""
+
+class Product(models.Model):
+    name = models.CharField(max_length=60)
+    price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.CharField(
+    max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/products/')
+
+    @staticmethod
+    def get_product_by_id(id):
+        return Product.objects.filter(id__in=id)
+
+    @staticmethod
+    def get_all_products():
+        return Product.objects.all()
+
+    @staticmethod
+    def get_all_products_by_categoryid(category_id):
+        if category_id:
+            return Product.objects.filter(category=category_id)
+        else:
+            return Product.objects.all()
+
+    def __str__(self):
+        return f"Product: \"{self.name}\""
+
