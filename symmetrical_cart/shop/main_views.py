94a1c1a563113325a31cd465707a12d7deb27b1e
ddvuser <denisdoroshenko99@gmail.com>
2023-12-09 18:36:53 +0200

    add pagination

--- symmetrical_cart/shop/main_views.py
+++ symmetrical_cart/shop/main_views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator
 
 def index(request):
+    products = Product.objects.get_all_products()
+    paginator = Paginator(products, 2)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
     context = {
-        'products': Product.objects.get_all_products()
+        "page_obj": page_obj
     }
     return render(request, "index.html", context=context)
 
