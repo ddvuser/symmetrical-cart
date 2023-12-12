b918f0fe67dc6423d40b1e9c06082ed49774eb46
ddvuser <denisdoroshenko99@gmail.com>
2023-12-12 17:02:51 +0200

    add cart template, favicon, cart quantity js

---
+++ symmetrical_cart/static/shop/cart.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var quantityIncrementButtons = document.querySelectorAll('#quantity-increment');
+    var quantityDecrementButtons = document.querySelectorAll('#quantity-decrement');
+
+    quantityIncrementButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var input = button.parentElement.querySelector('#quantity-input');
+            input.value = parseInt(input.value) + 1;
+        });
+    });
+
+    quantityDecrementButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var input = button.parentElement.querySelector('#quantity-input');
+            input.value = parseInt(input.value) - 1;
+        });
+    });
+});
