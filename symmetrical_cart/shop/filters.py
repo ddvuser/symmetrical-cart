2bd0ff25901aa01eb838788f710ee84c9ce69497
ddvuser <denisdoroshenko99@gmail.com>
2023-12-18 21:54:15 +0200

    change navbar collapse, add search bar

--- symmetrical_cart/shop/filters.py
+++ symmetrical_cart/shop/filters.py
@@ -5,7 +5,7 @@ class ProductFilter(django_filters.FilterSet):
     price__lt = django_filters.NumberFilter(field_name='price', lookup_expr='lt')
     price__gt = django_filters.NumberFilter(field_name='price', lookup_expr='gt')
     release_year = django_filters.NumberFilter(field_name='release__year', lookup_expr='exact')
-    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    search = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
     category = django_filters.CharFilter(field_name='category__name', lookup_expr='icontains')
 
     class Meta:
