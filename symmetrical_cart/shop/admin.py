832cf3807fb5bed46b08a753668a9f29ae913722
ddvuser <denisdoroshenko99@gmail.com>
2023-12-23 17:53:18 +0200

    add 404 page, add custom admin

--- symmetrical_cart/shop/admin.py
+++ symmetrical_cart/shop/admin.py
@@ -5,8 +5,12 @@ from .models import CustomUser, Category, Product, Order, OrderProduct
 
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
+    list_display = ("name", "category", "release")
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("email", "date_joined")
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Category)
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
