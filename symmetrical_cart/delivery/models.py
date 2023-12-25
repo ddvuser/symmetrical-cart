32758e850f26a6d4dd35331d61ba245f9e97ffb6
ddvuser <denisdoroshenko99@gmail.com>
2023-12-26 01:06:11 +0200

    add user field to employee model

--- symmetrical_cart/delivery/models.py
+++ symmetrical_cart/delivery/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from shop.models import Order
+from shop.models import Order, CustomUser
 
 class Employee(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     salary = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     is_available = models.BooleanField(default=True)
     ratings = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
@@ -13,7 +14,7 @@ class Employee(models.Model):
     delivered_orders = models.ManyToManyField(Order, related_name='delivered_delivery_user', blank=True)
 
     def __str__(self):
-        return f"DeliveryMan: {self.name} {self.surname}"
+        return f"DeliveryMan: {self.user.name} {self.user.surname}"
 
     class Meta:
         verbose_name = 'Delivery User'
